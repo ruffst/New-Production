256cf1c89f6c8d8354228699096037de0f00e31f
CaptainHarlock0006 <94286908+CaptainHarlock0006@users.noreply.github.com>
2021-11-27 18:43:53 +0100

    Research Tree

--- CreateBlocks.ps1
+++ CreateBlocks.ps1
@@ -1,23 +1,39 @@
 $BlockIds = @(
 
-  'Rohrextruder'
-  'Plattenpresse'
-  'BasicAssembler'
-  'Drahtextruder'
-  'Wickelmaschine'
-  'Elektro_Assembler'
-  'Fertiger'
-  'VerbrauchsmaterialAssembler'
-  'SurvivalKitLarge'
-  'BasicAssembler'
+  'Meilenstein_Armorlight'
+  'Meilenstein_Armorheavy'
+  'Meilenstein_ArmorDeko'
+  'Meilenstein_Fenster'
+  'Meilenstein_Cockpits'
+  'Meilenstein_Tools'
+  'Meilenstein_Waffen'
+  'Meilenstein_Conveyors'
+  'Meilenstein_Deko1'
+  'Meilenstein_Deko2'
+  'Meilenstein_Industrial'
+  'Meilenstein_Power'
+  'Meilenstein_Lights'
+  'Meilenstein_Thruster_Atmo'
+  'Meilenstein_Thruster_Hydro'
+  'Meilenstein_Thruster_Ionen'
+  'Meilenstein_LCDs'
+  'Meilenstein_Sound_Blocks'
+  'Meilenstein_Gravity'
+  'Meilenstein_Mechanic'
+  'Meilenstein_Tore'
+  'Meilenstein_Räder'
+  'Meilenstein_Cargos'
+  'Meilenstein_Jumpdrive'
+  'Meilenstein_Communication'
+  'Meilenstein_Landing'
 
 );
-$Builder = "CubeBlock"
+$Builder = "Conveyor"
 $Gruppe = "0"
 
 $TextBlock = '
-    <ResearchBlock xsi:type="ResearchBlock">                                    <!--XXXXXXXX  Gruppe zzzzz-->
-      <Id Type="MyObjectBuilder_yyyyy" Subtype="XXXXXXXX" />
+    <ResearchBlock xsi:type="ResearchBlock">                                    <!--XXXXX  zzzzz-->
+      <Id Type="MyObjectBuilder_yyyyy" Subtype="XXXXX" />
       <UnlockedByGroups>
         <GroupSubtype>zzzzz</GroupSubtype>
       </UnlockedByGroups>
@@ -25,7 +41,7 @@ $TextBlock = '
 
 foreach ($Block in $BlockIds)
 {
-$temp = $TextBlock -replace "XXXXXXXX", $Block
+$temp = $TextBlock -replace "XXXXX", $Block
 $temp = $temp -replace "yyyyy", $Builder
 $temp = $temp -replace "zzzzz", $Gruppe
 Write-Host $temp
