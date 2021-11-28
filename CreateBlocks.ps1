276c73fcb1860b768002b8c67aa4d7e7a2d399ec
CaptainHarlock0006 <94286908+CaptainHarlock0006@users.noreply.github.com>
2021-11-28 11:55:59 +0100

    Tree erweiterung

--- CreateBlocks.ps1
+++ CreateBlocks.ps1
@@ -1,49 +1,36 @@
 $BlockIds = @(
 
-  'Meilenstein_Armorlight'
-  'Meilenstein_Armorheavy'
-  'Meilenstein_ArmorDeko'
-  'Meilenstein_Fenster'
-  'Meilenstein_Cockpits'
-  'Meilenstein_Tools'
-  'Meilenstein_Waffen'
-  'Meilenstein_Conveyors'
-  'Meilenstein_Deko1'
-  'Meilenstein_Deko2'
-  'Meilenstein_Industrial'
-  'Meilenstein_Power'
-  'Meilenstein_Lights'
-  'Meilenstein_Thruster_Atmo'
-  'Meilenstein_Thruster_Hydro'
-  'Meilenstein_Thruster_Ionen'
-  'Meilenstein_LCDs'
-  'Meilenstein_Sound_Blocks'
-  'Meilenstein_Gravity'
-  'Meilenstein_Mechanic'
-  'Meilenstein_Tore'
-  'Meilenstein_Räder'
-  'Meilenstein_Cargos'
-  'Meilenstein_Jumpdrive'
-  'Meilenstein_Communication'
-  'Meilenstein_Landing'
+  'LargeBlockCockpitSeat'
+  'SmallBlockCockpit'
+  'DBSmallBlockFighterCockpit'
+  'SmallBlockCockpitIndustrial'
+  'LargeBlockCockpitIndustrial'
+  'LargeBlockCockpit'
+  'CockpitOpen'
+  'OpenCockpitLarge'
+  'OpenCockpitSmall'
+  'RoverCockpit'
+  'BuggyCockpit'
+  'PassengerSeatLarge'
+  'PassengerSeatSmall'
 
 );
-$Builder = "Conveyor"
-$Gruppe = "0"
+$Builder = "Cockpit"
+$Freigeschaltet = "Meilenstein_Controls"
 
 $TextBlock = '
-    <ResearchBlock xsi:type="ResearchBlock">                                    <!--XXXXX  zzzzz-->
-      <Id Type="MyObjectBuilder_yyyyy" Subtype="XXXXX" />
-      <UnlockedByGroups>
-        <GroupSubtype>zzzzz</GroupSubtype>
-      </UnlockedByGroups>
-    </ResearchBlock>'
+        <ResearchBlock xsi:type="ResearchBlock">                                    <!--XXXXX  zzzzz-->
+          <Id Type="MyObjectBuilder_yyyyy" Subtype="XXXXX" />
+          <UnlockedByGroups>
+            <GroupSubtype>zzzzz</GroupSubtype>
+          </UnlockedByGroups>
+        </ResearchBlock>'
 
 foreach ($Block in $BlockIds)
 {
 $temp = $TextBlock -replace "XXXXX", $Block
 $temp = $temp -replace "yyyyy", $Builder
-$temp = $temp -replace "zzzzz", $Gruppe
+$temp = $temp -replace "zzzzz", $Freigeschaltet
 Write-Host $temp
 
 }
