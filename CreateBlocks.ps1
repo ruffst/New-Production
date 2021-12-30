0682294a9e46b3d1cb9d167c435d71c7365e4a55
CaptainHarlock0006 <94286908+CaptainHarlock0006@users.noreply.github.com>
2021-12-30 22:06:09 +0100

    Fehler Behebung Small Blocks

--- CreateBlocks.ps1
+++ CreateBlocks.ps1
@@ -3,8 +3,51 @@ $BlockIds = @(
 
 
 
-  'SmallBlockConveyorBasic'
-  'LargeBlockConveyorBasic'
+  'LargeGridBeamBlockTJunction'
+  'SmallGridBeamBlock'
+  'SmallGridBeamBlockSlope'
+  'SmallGridBeamBlockRound'
+  'SmallGridBeamBlockSlope2x1Base'
+  'SmallGridBeamBlockSlope2x1Tip'
+  'SmallGridBeamBlockHalf'
+  'SmallGridBeamBlockHalfSlope'
+  'SmallGridBeamBlockEnd'
+  'SmallGridBeamBlockJunction'
+  'SmallGridBeamBlockTJunction'
+
+  'SmallArmorPanelLight'
+  'SmallArmorSlopedSidePanelLight'
+  'SmallArmorSlopedPanelLight'
+  'SmallArmorHalfPanelLight'
+  'SmallArmorQuarterPanelLight'
+  'SmallArmor2x1SlopedPanelLight'
+  'SmallArmor2x1SlopedPanelTipLight'
+  'SmallArmor2x1SlopedSideBasePanelLight'
+  'SmallArmor2x1SlopedSideTipPanelLight'
+  'SmallArmor2x1SlopedSideBasePanelLightInv'
+  'SmallArmor2x1SlopedSideTipPanelLightInv'
+  'SmallArmorHalfSlopedPanelLight'
+  'SmallArmor2x1HalfSlopedPanelLightRight'
+  'SmallArmor2x1HalfSlopedTipPanelLightRight'
+  'SmallArmor2x1HalfSlopedPanelLightLeft'
+  'SmallArmor2x1HalfSlopedTipPanelLightLeft'
+
+  'SmallArmorPanelHeavy'
+  'SmallArmorSlopedSidePanelHeavy'
+  'SmallArmorSlopedPanelHeavy'
+  'SmallArmorHalfPanelHeavy'
+  'SmallArmorQuarterPanelHeavy'
+  'SmallArmor2x1SlopedPanelHeavy'
+  'SmallArmor2x1SlopedPanelTipHeavy'
+  'SmallArmor2x1SlopedSideBasePanelHeavy'
+  'SmallArmor2x1SlopedSideTipPanelHeavy'
+  'SmallArmor2x1SlopedSideBasePanelHeavyInv'
+  'SmallArmor2x1SlopedSideTipPanelHeavyInv'
+  'SmallArmorHalfSlopedPanelHeavy'
+  'SmallArmor2x1HalfSlopedPanelHeavyRight'
+  'SmallArmor2x1HalfSlopedTipPanelHeavyRight'
+  'SmallArmor2x1HalfSlopedPanelHeavyLeft'
+  'SmallArmor2x1HalfSlopedTipPanelHeavyLeft'
 
 
 
@@ -13,8 +56,8 @@ $BlockIds = @(
 
 
 );
-$Builder = "Conveyor"
-$Freigeschaltet = "Meilenstein_Basic"
+$Builder = "CubeBlock"
+$Freigeschaltet = "Meilenstein_ArmorDeko"
 
 $TextBlock = '
         <ResearchBlock xsi:type="ResearchBlock">                                    <!--XXXXX  zzzzz-->
